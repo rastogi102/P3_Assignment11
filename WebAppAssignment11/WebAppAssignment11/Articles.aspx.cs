d9838a04afd0f4a78c39124d065dac3b43313850
rastogi102 <nikhilrastogi102@gmail.com>
2023-08-25 16:06:23 +0530

    initial commit

---
+++ WebAppAssignment11/WebAppAssignment11/Articles.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppAssignment11
+{
+    public partial class Articles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=DESKTOP-MFQ8M0P;Database=ContentDB;Trusted_Connection=true;");
+                SqlCommand cmd = new SqlCommand("Select * From Articles", con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                ArticleTbl.DataSource = ds;
+                ArticleTbl.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Lbl.Text = ex.Message;
+            }
+        }
+    }
+}
---
+++ WebAppAssignment11/WebAppAssignment11/Home.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site Master.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="WebAppAssignment11.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <h2 class="text-center" style="font-family: Ebrima; font-size: larger; font-weight: bold">Home</h2>
+
+</asp:Content>
